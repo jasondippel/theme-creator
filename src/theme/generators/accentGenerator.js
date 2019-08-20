224c7d791ccbae76ccdbe1aa5c784d211b41eb47
jasondippel <dippelj12@gmail.com>
2019-08-20 10:42:08 -0700

    compute accent colors

--- src/theme/generators/accentGenerator.js
+++ src/theme/generators/accentGenerator.js
@@ -1,11 +1,66 @@
-import { getTheme } from '../utils'
+import { getTheme, hexToHsl, hslToHex } from '../utils'
+
+/**
+ * TODO: Consider mixing HSL colors in V2
+ * https://stackoverflow.com/questions/35816179/calculation-algorithm-to-mix-3-hsl-colors
+ **/
+
+const H = 0
+const S = 1
+const L = 2
+
+const computeAccent = color => {
+  const colorHsl = hexToHsl(color)
+
+  console.log('hsl value', colorHsl)
+
+  const accentHsl = [
+    colorHsl[H],
+    Math.min(
+      colorHsl[L] < 50
+        ? colorHsl[S] - colorHsl[S] * 0.1
+        : colorHsl[S] + colorHsl[S] * 0.1,
+      100,
+    ),
+    Math.min(
+      colorHsl[L] < 50
+        ? colorHsl[L] + (6 * (100 - colorHsl[L])) / 9
+        : colorHsl[L] + (4 * (100 - colorHsl[L])) / 9,
+      100,
+    ),
+  ]
+
+  return hslToHex(...accentHsl)
+}
+
+const computeBackground = color => {
+  const colorHsl = hexToHsl(color)
+  const accentHsl = [
+    colorHsl[H],
+    Math.min(
+      colorHsl[L] < 50
+        ? colorHsl[S] - colorHsl[S] * 0.1
+        : colorHsl[S] + colorHsl[S] * 0.1,
+      100,
+    ),
+    Math.min(
+      colorHsl[L] < 50
+        ? colorHsl[L] + colorHsl[L] * 0.4 + (100 - colorHsl[L]) / 4
+        : colorHsl[L] + colorHsl[L] * 0.4,
+      100,
+    ),
+  ]
+
+  return hslToHex(...accentHsl)
+}
 
 export const generatePrimaryAccents = primary => {
   const $theme = getTheme()
 
+  console.log('computing primary')
   return {
     primary,
-    primaryAccent: $theme.primaryAccent,
+    primaryAccent: computeAccent(primary),
     primaryBackground: $theme.primaryBackground,
   }
 }
@@ -13,9 +68,10 @@ export const generatePrimaryAccents = primary => {
 export const generateSuccessAccents = success => {
   const $theme = getTheme()
 
+  console.log('computing success')
   return {
     success,
-    successAccent: $theme.successAccent,
+    successAccent: computeAccent(success),
     successBackground: $theme.successBackground,
   }
 }
@@ -23,18 +79,20 @@ export const generateSuccessAccents = success => {
 export const generateWarningAccents = warning => {
   const $theme = getTheme()
 
+  console.log('computing warning')
   return {
     warning,
-    warningAccent: $theme.warningAccent,
+    warningAccent: computeAccent(warning),
     warningBackground: $theme.warningBackground,
   }
 }
 export const generateErrorAccents = error => {
   const $theme = getTheme()
 
+  console.log('computing error')
   return {
     error,
-    errorAccent: $theme.errorAccent,
+    errorAccent: computeAccent(error),
     errorBackground: $theme.errorBackground,
   }
 }
